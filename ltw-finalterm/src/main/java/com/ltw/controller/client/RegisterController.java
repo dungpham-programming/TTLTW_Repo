b6eb7858b8a41c22b53714530d1f8f40329b4aff
dungpham-programming <21130326@st.hcmuaf.edu.vn>
2023-12-03 11:14:30 +0700

    Thêm Controller và JSP cho signup (register), sửa dổi lại src

--- ltw-finalterm/src/main/java/com/ltw/controller/client/RegisterController.java
+++ ltw-finalterm/src/main/java/com/ltw/controller/client/RegisterController.java
@@ -7,11 +7,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(value = {"/home"})
-public class HomeController extends HttpServlet {
+@WebServlet(value = {"/register"})
+public class RegisterController extends HttpServlet {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.getRequestDispatcher("client-home.jsp").forward(req, resp);
+        req.getRequestDispatcher("signup.jsp").forward(req, resp);
     }
 
     @Override
