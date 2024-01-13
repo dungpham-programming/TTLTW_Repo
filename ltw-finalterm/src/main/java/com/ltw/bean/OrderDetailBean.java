c8b7339338eb8bd696e81175b93f711ddc15f103
dungpham-programming <21130326@st.hcmuaf.edu.vn>
2024-01-14 00:45:50 +0700

    Code sơ lược khung của Chi tiết đơn hàng

---
+++ ltw-finalterm/src/main/java/com/ltw/bean/OrderDetailBean.java
@@ -0,0 +1,34 @@
+package com.ltw.bean;
+
+public class OrderDetailBean {
+    private int orderId;
+    private int productId;
+    private int quantity;
+
+    public OrderDetailBean() {
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getProductId() {
+        return productId;
+    }
+
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
---
+++ ltw-finalterm/src/main/java/com/ltw/controller/admin/order_detail/OrderDetailManagementController.java
@@ -0,0 +1,17 @@
+package com.ltw.controller.admin.order_detail;
+
+import com.ltw.util.BlankInputUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@WebServlet(value = {"/admin/order-detail-management"})
+public class OrderDetailManagementController extends HttpServlet {
+
+}
