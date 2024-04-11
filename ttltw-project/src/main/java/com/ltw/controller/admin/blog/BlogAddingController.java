e4324685ec5d6399ea14359d13d584bda2354326
NDsPr <21130336@st.hcmuaf.edu.vn>
2024-04-11 13:37:20 +0700

    hoàn thành chức năng thêm blog
    viết các lớp jsp, BlogAddingController,thêm phương thức trong lớp BlogDAO sẵn có

--- ttltw-project/src/main/java/com/ltw/controller/admin/blog/BlogAddingController.java
+++ ttltw-project/src/main/java/com/ltw/controller/admin/blog/BlogAddingController.java
@@ -1,10 +1,79 @@
 package com.ltw.controller.admin.blog;
 
+import com.ltw.bean.BlogBean;
+import com.ltw.bean.UserBean;
+import com.ltw.dao.BlogDAO;
+import com.ltw.dao.UserDAO;
+import com.ltw.util.BlankInputUtil;
+import com.ltw.util.EncryptPasswordUtil;
+import com.ltw.util.NumberValidateUtil;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 
 
 @WebServlet("/admin/blog-management/adding")
 
 public class BlogAddingController extends HttpServlet {
+
+    private final BlogDAO blogDAO = new BlogDAO();
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/adding-blog.jsp").forward(req, resp);
+    }
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+
+        String title = req.getParameter("title");
+        String description = req.getParameter("description");
+        String content = req.getParameter("content");
+        String categoryId = req.getParameter("categoryId");
+        String profilePic = req.getParameter("profilePic");
+        String status = req.getParameter("status");
+
+        String success = "success";
+        String[] inputsForm = new String[] {title, description, content, categoryId,profilePic, status};
+        ArrayList<String> errors = new ArrayList<>();
+        // Biến bắt lỗi
+        boolean isValid = true;
+
+        for (String string : inputsForm) {
+            if (BlankInputUtil.isBlank(string)) {
+                errors.add("e");
+                if (isValid) {
+                    isValid = false;
+                }
+            } else {
+                errors.add(null);
+            }
+        }
+        req.setAttribute("errors", errors);
+
+        // Nếu không lỗi thì lưu vào database
+        if (isValid) {
+            // Đổi String về số
+            int statusInt = NumberValidateUtil.toInt(status);
+            int categoryIdInt = NumberValidateUtil.toInt(categoryId);
+            // Set thuộc tính vào bean
+            BlogBean blogBean = new BlogBean();
+            blogBean.setTitle(title);
+            blogBean.setDescription(description);
+            blogBean.setContent(content);
+            blogBean.setCategoryId(categoryIdInt);
+            blogBean.setProfilePic(profilePic);
+            blogBean.setStatus(statusInt);
+
+
+            blogDAO.createAccount(blogBean);
+            resp.sendRedirect(req.getContextPath() + "/admin/blog-management/adding?success=" + success);
+        } else {
+            req.getRequestDispatcher("/adding-blog.jsp").forward(req, resp);
+        }
+    }
 }
+
+
