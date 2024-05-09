4be9ce59875a7351b88c8b43597d96a03d2a8da8
NDsPr <21130336@st.hcmuaf.edu.vn>
2024-05-09 21:19:05 +0700

    tạo createReview()
    trong ReviewDAO, hoàn thành phương thức createReview() để thêm cào data các đánh giá

--- ttltw-project/src/main/java/com/ltw/dao/ReviewDAO.java
+++ ttltw-project/src/main/java/com/ltw/dao/ReviewDAO.java
@@ -17,7 +17,7 @@ public class ReviewDAO {
     public List<ReviewBean> findAllReviews() {
         String sql = "SELECT id, productId, userId, username, " +
                 "content, rating, helpfulCount, reportCount, " +
-                "createdDate, modifiedDate " +
+                "createdDate,createdBy, modifiedDate,modifiedBy " +
                 "FROM reviews";
 
         List<ReviewBean> reviewList = new ArrayList<>();
@@ -42,7 +42,9 @@ public class ReviewDAO {
                 reviewBean.setHelpfulCount(resultSet.getInt("helpfulCount"));
                 reviewBean.setReportCount(resultSet.getInt("reportCount"));
                 reviewBean.setCreatedDate(resultSet.getTimestamp("createdDate"));
+                reviewBean.setCreatedBy(resultSet.getString("createdBy"));
                 reviewBean.setModifiedDate(resultSet.getTimestamp("modifiedDate"));
+                reviewBean.setModifiedBy(resultSet.getString("modifiedBy"));
 
                 reviewList.add(reviewBean);
             }
@@ -54,31 +56,33 @@ public class ReviewDAO {
         return reviewList;
     }
 
-//    public void createReview(ReviewBean reviewBean) {
-//        String sql = "INSERT INTO users (password, firstName, lastName, roleId, " +
-//                "email, addressLine, addressWard, addressDistrict, status) " +
-//                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-//
-//        Connection connection = null;
-//        PreparedStatement preparedStatement = null;
-//
-//        try {
-//            connection = OpenConnectionUtil.openConnection();
-//            connection.setAutoCommit(false);
-//            preparedStatement = connection.prepareStatement(sql);
-//            SetParameterUtil.setParameter(preparedStatement, userBean.getPassword(), userBean.getFirstName(),
-//                    userBean.getLastName(), userBean.getRoleId(), userBean.getEmail(), userBean.getAddressLine(),
-//                    userBean.getAddressWard(), userBean.getAddressDistrict(), userBean.getStatus());
-//            preparedStatement.executeUpdate();
-//            connection.commit();
-//        } catch (SQLException e) {
-//            try {
-//                connection.rollback();
-//            } catch (SQLException ex) {
-//                throw new RuntimeException(ex);
-//            }
-//        } finally {
-//            CloseResourceUtil.closeNotUseRS(preparedStatement, connection);
-//        }
-//    }
+    public void createReview(ReviewBean reviewBean) {
+        String sql = "INSERT INTO reviews (productId, userId, username, " +
+                "content, rating, helpfulCount, reportCount, " +
+                "createdDate,createdBy, modifiedDate,modifiedBy "  +
+                "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+
+        try {
+            connection = OpenConnectionUtil.openConnection();
+            connection.setAutoCommit(false);
+            preparedStatement = connection.prepareStatement(sql);
+            SetParameterUtil.setParameter(preparedStatement, reviewBean.getProductId(), reviewBean.getUserId(),
+                    reviewBean.getUsername(), reviewBean.getContent(), reviewBean.getRating(), reviewBean.getHelpfulCount(),
+                    reviewBean.getReportCount(), reviewBean.getCreatedDate(), reviewBean.getCreatedBy(),
+                    reviewBean.getModifiedDate(),reviewBean.getModifiedBy());
+            preparedStatement.executeUpdate();
+            connection.commit();
+        } catch (SQLException e) {
+            try {
+                connection.rollback();
+            } catch (SQLException ex) {
+                throw new RuntimeException(ex);
+            }
+        } finally {
+            CloseResourceUtil.closeNotUseRS(preparedStatement, connection);
+        }
+    }
 }
