af209a9c0eb87a6148c41f3431df3c8f0981582e
NDsPr <21130336@st.hcmuaf.edu.vn>
2024-07-05 19:10:48 +0700

    Tạo DT-blog với các thuộc tính cần thiết
    thiết lập colums

---
+++ ttltw-project/src/main/webapp/templates/logic-datatable/datatable-blog.js
@@ -0,0 +1,28 @@
+$(document).ready(function (){
+    $('blogData').DataTable({
+        lengthMenu: [5, 10, 25, 100],
+        processing: true,
+        serverSide: true,
+        ajax: {
+            url:"http://localhost:8080/api/admin/blog",
+            type: "POST",
+            dataType: "json",
+            dataSrc: "items",        // Dữ liệu cần lấy từ thuộc tính JSON, ở đây là items (Danh sách)
+        },
+        columns:[
+            { data: "id"},
+            { data: "title"},
+            { data: "author"},
+            { data: "description"},
+            { data: "content"},
+            { data: "categoryId"},
+            { data: "status"},
+            { data: "profilePic"},
+            { data: "createdDate"},
+            // Thiết lập cho cột Thao tác
+            {sortable: false, searchable: false,}
+        ],
+    })
+    }
+
+)
